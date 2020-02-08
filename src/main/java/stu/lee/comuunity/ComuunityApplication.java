a88703d5992e50cae81f1976215b0ffbb5152309
Kalrs <1126597277@qqc.om>
2020-02-08 16:55:11 +0800

    init repo

---
+++ src/main/java/stu/lee/comuunity/ComuunityApplication.java
@@ -0,0 +1,13 @@
+package stu.lee.comuunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComuunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stu/lee/comuunity/ComuunityApplicationTests.java
@@ -0,0 +1,13 @@
+package stu.lee.comuunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
